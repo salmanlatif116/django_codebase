5c95696af529f98002613df9106db7b69ec8a8c3
salman latif <salu33956@gmail.com>
2025-03-12 15:31:26 +0500

    initial commit

---
+++ main/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('POSTGRES_ENGINE'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('PG_HOST'),
+        'PORT': env('PG_PORT'),
+    }
+}
+
+
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER = '5ee6103eeabb7e'
+EMAIL_HOST_PASSWORD = 'f7e5ce42460a6b'
+EMAIL_PORT = '2525'
